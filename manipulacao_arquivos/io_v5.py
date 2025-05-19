dca008c044b9fd4b8a9d8c1887b7a52b983bd1ef
alessandrocecilio <alessandro.m.cecilio@gmail.com>
2025-05-19 12:20:05 -0300

    Commit Referente a Manipulação de Arquivos

---
+++ manipulacao_arquivos/io_v5.py
@@ -0,0 +1,14 @@
+#!python
+
+with open('pessoas.csv') as arquivo:
+    with open('pessoas.txt', 'w') as saida:
+        for registro in arquivo:
+            pessoa = registro.strip().split(',')
+            print('Nome : {},  Idade : {}'.format(*pessoa), file = saida)
+
+    
+if arquivo.close:
+    print('O Arquivo foi fechado!')
+
+if saida.close:
+    print('O Arquivo de saida foi fechado!')
