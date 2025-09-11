d6a4845618b6501c74d00a428a6f65dee33d664a
alessandrocecilio <alessandro.m.cecilio@gmail.com>
2025-09-11 12:47:25 -0300

    Módulo Sobre Revisão de SQL

---
+++ banco-relacional/inserirEstados2.sql
@@ -0,0 +1,27 @@
+USE cp;
+INSERT INTO estados 
+    (nome, sigla, regiao, populacao) 
+VALUES
+    ('Bahia', 'BA', 'Nordeste', 14.87),
+    ('Ceará', 'CE', 'Nordeste', 9.24),
+    ('Distrito Federal', 'DF', 'Centro-Oeste', 3.05),
+    ('Espírito Santo', 'ES', 'Sudeste', 4.06),
+    ('Goiás', 'GO', 'Centro-Oeste', 7.11),
+    ('Maranhão', 'MA', 'Nordeste', 7.15),
+    ('Mato Grosso', 'MT', 'Centro-Oeste', 3.44),
+    ('Mato Grosso do Sul', 'MS', 'Centro-Oeste', 2.84),
+    ('Minas Gerais', 'MG', 'Sudeste', 21.21),
+    ('Pará', 'PA', 'Norte', 8.77),
+    ('Paraíba', 'PB', 'Nordeste', 4.06),
+    ('Paraná', 'PR', 'Sul', 11.52),
+    ('Pernambuco', 'PE', 'Nordeste', 9.62),
+    ('Piauí', 'PI', 'Nordeste', 3.27),
+    ('Rio de Janeiro', 'RJ', 'Sudeste', 17.36),
+    ('Rio Grande do Norte', 'RN', 'Nordeste', 3.56),
+    ('Rio Grande do Sul', 'RS', 'Sul', 11.33),
+    ('Rondônia', 'RO', 'Norte', 1.8),
+    ('Roraima', 'RR', 'Norte', 0.63),
+    ('Santa Catarina', 'SC', 'Sul', 7.35),
+    ('São Paulo', 'SP', 'Sudeste', 46.29),
+    ('Sergipe', 'SE', 'Nordeste', 2.32),
+    ('Tocantins', 'TO', 'Norte', 1.57);
