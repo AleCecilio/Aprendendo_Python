b524e410c8f6169cbdc6a81f43d7dfbfa2fc12ee
alessandrocecilio <alessandro.m.cecilio@gmail.com>
2025-09-15 12:37:32 -0300

    Inicio Do Módulo de Banco de Dados com Python

---
+++ banco_dados/excluir_tabela.py
@@ -0,0 +1,11 @@
+from bd import nova_conexao
+from mysql.connector import ProgrammingError
+
+
+try:
+    with nova_conexao() as conexao:
+        try:
+            cursor =  conexao.cursor()
+            cursor.execute('DROP TABLE IF EXISTS emails')
+        except ProgrammingError as e:
+            print(f'Erro {e.msg}')    
